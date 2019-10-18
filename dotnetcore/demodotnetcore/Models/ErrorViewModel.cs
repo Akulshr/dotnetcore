460598df1e05501041201dda03a914ea591a0667
Anubhav Kulshrestha <anubhav.kulshrestha@azuresubdocker.onmicrosoft.com>
2019-10-18 12:38:06 -0500

    Comit

---
+++ dotnetcore/demodotnetcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace demodotnetcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
